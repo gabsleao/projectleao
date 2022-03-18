2a11f67f9e5b08adfabdf59f53f23de188acd5df
Gabriel Silva <gabidonn@gmail.com>
2022-03-18 20:16:45 -0300

    remove unused logs

--- index.php
+++ index.php
@@ -3,8 +3,6 @@
 require_once __DIR__.'/config/head.php';
 require_once __DIR__.'/config/loaders.php';
 
-Log::doLog(isset($_SESSION) ? var_export($_SESSION, 1) : 'not set', 'SessionsAfterSet3');
-
 if(!isset($_SESSION['iduser']))
     require_once __DIR__.'/login_page.php';
 else
